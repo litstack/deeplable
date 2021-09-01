4d4c8364980d0dfee02acd5462bbb9ad9bb494db
cbl <lennart.carbe@gmail.com>
2021-09-01 13:23:25 +0200

    wip

---
+++ src/TranslateAllAction.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Litstack\Deeplable;
+
+use Ignite\Support\AttributeBag;
+use Illuminate\Support\Collection;
+use Ignite\Page\Actions\ActionModal;
+use Ignite\Support\Vue\ButtonComponent;
+use Illuminate\Support\Facades\Artisan;
+use AwStudio\Deeplable\Facades\Translator;
+
+class TranslateAllAction
+{
+    public function modal(ActionModal $modal)
+    {
+        $modal->title(__lit('deeplable.translate_deepl'))->confirmText(__lit('deeplable.translate'));
+        $modal->form(function ($form) {
+            $form
+                ->select('locale')
+                ->title(__lit('deeplable.language'))
+                ->hint(__lit('deeplable.messages.overwrite_warning'))
+                ->options(
+                    collect(config('translatable.locales'))
+                        ->filter(fn ($locale) => $locale != config('translatable.fallback_locale'))
+                        ->mapWithKeys(fn ($locale) => [$locale => $locale])
+                        ->toArray()
+                );
+        });
+    }
+
+    public function run(Collection $models, AttributeBag $attributes)
+    {
+        $targetLanguage = $attributes->locale;
+        $sourceLanguage = config('translatable.fallback_locale');
+
+        if (! $targetLanguage) {
+            return response()->danger(__lit('deeplable.messages.missing_locale'));
+        }
+
+        Artisan::call('deeplable:run', ['locale' => $targetLanguage]);
+
+        return response()->success(__lit(
+            'deeplable.messages.translated',
+            [
+                'target' => $targetLanguage,
+                'source' => $sourceLanguage,
+            ]
+        ));
+    }
+}
