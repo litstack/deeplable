40354c3b9e6bacbe4ea1333a2caac663e756f721
cbl <lennart.carbe@gmail.com>
2021-09-08 12:03:48 +0200

    fix

--- src/FormModelTranslator.php
+++ src/FormModelTranslator.php
@@ -3,33 +3,37 @@
 namespace Litstack\Deeplable;
 
 use AwStudio\Deeplable\Facades\Translator;
-use Illuminate\Database\Eloquent\Model;
 use AwStudio\Deeplable\Translators\BaseTranslator;
+use Closure;
 use Ignite\Crud\Models\LitFormModel;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 class FormModelTranslator extends BaseTranslator
 {
     /**
      * Translate the given model attribute.
      *
-     * @param Model $model
-     * @param string $attribute
-     * @param string $locale
-     * @param string $translation
+     * @param  Model  $model
+     * @param  string $attribute
+     * @param  string $locale
+     * @param  string $translation
      * @return void
      */
     protected function translateAttribute(Model $model, $attribute, $locale, $translation, bool $force = true)
     {
-        $value = $model->getAttribute($attribute);
-        
+        $value = $this->withLocale(
+            $locale,
+            fn () => $model->getAttribute($attribute)
+        );
+
         if (! $force && $value && ! is_object($value)) {
             return;
         }
 
         if (! is_object($value)) {
             $model->update([$locale => [
-                $attribute => $translation
+                $attribute => $translation,
             ]]);
         } elseif ($value instanceof Collection) {
             foreach ($value as $child) {
@@ -42,10 +46,29 @@ class FormModelTranslator extends BaseTranslator
     }
 
     /**
+     * Temporary set locale.
+     *
+     * @param  string  $locale
+     * @param  Closure $clousre
+     * @return void
+     */
+    protected function withLocale($locale, Closure $closure)
+    {
+        $originalLocale = app()->getLocale();
+        app()->setLocale($locale);
+
+        $value = $closure();
+
+        app()->setLocale($originalLocale);
+
+        return $value;
+    }
+
+    /**
      * Get a list of the translated attributes of a model.
      *
-     * @param Model $model
-     * @param string $locale
+     * @param  Model  $model
+     * @param  string $locale
      * @return array
      */
     public function getTranslatedAttributes(Model $model, $locale)
